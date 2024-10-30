c2f50f81b705fb0cd8aeeb37fc8498cd2502e8a5
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2024-10-30 16:13:49 +0700

    Refactor .gitignore and add package.json, main.py, and server.js files

---
+++ main.py
@@ -0,0 +1,19 @@
+import os, sys, threading
+
+def run_flask():
+    os.system("python app.py")
+
+def run_express():
+    os.system("node server.js")
+
+# Check if user run the main.py file
+if __name__ == "__main__":
+    threading.Thread(target=run_flask).start()
+    threading.Thread(target=run_express).start()
+    
+    # Check keyboard interrupt
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        sys.exit(0)
